40f6e7ed50988c1a5d000c08dbcf7784f67823ba
Nguyendinhquan2605 <dinhquana12@gmail.com>
2024-03-15 11:08:33 +0700

    Hinhtron

--- BaiThucHanh/src/App.java
+++ BaiThucHanh/src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Employee nv = new Employee() ;
+        nv.Nhapthongtin();
+        nv.Tinhluongphucap();
+        nv.TinhLuongTB();
+        nv.Xuatthongtin();
     }
 }
