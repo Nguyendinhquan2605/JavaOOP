667e5362f5980993490792d77a3a19194bd081a5
Nguyendinhquan2605 <dinhquana12@gmail.com>
2024-04-26 23:01:13 +0700

    abstract

---
+++ BaiThucHanh/src/mainPrintable.java
@@ -0,0 +1,6 @@
+public class mainPrintable {
+    public static void main(String[] args) {
+        printable obj = new A6() ;
+        obj.print();;
+    }
+}
---
+++ BaiThucHanh/src/printable.java
@@ -0,0 +1,3 @@
+public interface printable {
+    public  abstract void  print() ;
+}
